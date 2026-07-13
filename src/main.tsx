d587f54dab808c7e3bcabd6bf0d78af79d070b44
Sathishkumar871 <sathishkumar533289@gmail.com>
2026-07-13 17:04:48 +0530

    Added CocoFresh AI chatbot with premium floating button

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
+import "@fortawesome/fontawesome-free/css/all.min.css"; // <-- Add this line
+
 import "./styles/global.css";
 import "./styles/variables.css";
 import "./styles/responsive.css";
